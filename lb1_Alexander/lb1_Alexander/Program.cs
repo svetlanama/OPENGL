69abd88e27535d891b9e644e3902769e8bc82897
Moiseyenko Svitlana <alexandrovna.sveta@gmail.com>
2016-11-12 18:19:55 +0200

    Change background color

--- lb1_Alexander/lb1_Alexander/Program.cs
+++ lb1_Alexander/lb1_Alexander/Program.cs
@@ -14,7 +14,7 @@ namespace lb1_Alexander
     {
         static void Initialization()
         {
-            Gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            Gl.glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
             Gl.glShadeModel(Gl.GL_FLAT);
             Gl.glEnable(Gl.GL_LINE_SMOOTH);
             Gl.glHint(Gl.GL_LINE_SMOOTH_HINT, Gl.GL_NICEST);
