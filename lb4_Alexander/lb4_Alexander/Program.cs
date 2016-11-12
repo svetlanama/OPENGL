671c4a09ea79f4681a2e1fe86f2251abbc543746
Moiseyenko Svitlana <alexandrovna.sveta@gmail.com>
2016-11-13 00:38:33 +0200

    lb4 Alexander

--- lb4_Alexander/lb4_Alexander/Program.cs
+++ lb4_Alexander/lb4_Alexander/Program.cs
@@ -11,36 +11,49 @@ namespace lb4_Alexander
     class Program
     {
         private static int _widowSize = 800;
-        private static float[] ambient = { 1.0f, 0.0f, 0.0f, 0.5f };
-        private static float[] mat_diffuse = { 1.0f, 0.0f, 0.0f, 0.0f };
+        private static float[] ambient = { 0.0f, 0.0f, 0.0f, 1.0f };
+        private static float[] diffuse = { 1.0f, 0.0f, 0.0f, 0.1f };
+        private static float[] specular = { 0.2f, 0.1f, 0.1f, 0.1f };
+        private static float[] emission = { 0.0f, 0.0f, 0.0f, 1.0f };
 
+        private static float  shininess = 1.09f;
+
+        private static float[] light1 = { 0, 0, 4 };
+        private static float[] light2 = { 0, 0, -4 };
+
+        private static float _angleSphereY = 0.0f;
+        private static int _mlsec = 500;
 
         static void InitializeViewAppearance()
         {
-            Gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            Gl.glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
 
-            Gl.glClearDepth(1.0f);                   // Set background depth to farthest
+            Gl.glClearDepth(10.0f);          // Усиановка глубины
             Gl.glEnable(Gl.GL_DEPTH_TEST);   // Enable depth testing for z-culling
-            Gl.glDepthFunc(Gl.GL_LEQUAL);    // Set the type of depth-test
+            Gl.glDepthFunc(Gl.GL_LEQUAL);    // Тип глубины
             Gl.glShadeModel(Gl.GL_SMOOTH);
 
             Gl.glHint(Gl.GL_PERSPECTIVE_CORRECTION_HINT, Gl.GL_NICEST);
 
+            Gl.glEnable(Gl.GL_LINE_SMOOTH);
+            Gl.glHint(Gl.GL_LINE_SMOOTH_HINT, Gl.GL_NICEST);
+
             Gl.glEnable(Gl.GL_LIGHTING);
             Gl.glEnable(Gl.GL_LIGHT0);
+            Gl.glEnable(Gl.GL_LIGHT1);
         }
 
         static void CreateWindow()
         {
             Glut.glutInit();
-            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_RGB);
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_RGBA);
             Glut.glutInitWindowSize(_widowSize, _widowSize);
-            Glut.glutCreateWindow("LB1: Piramid");
+            Glut.glutCreateWindow("LB4: Glass sphere");
             InitializeViewAppearance();
 
             Glut.glutReshapeFunc(Reshape);
             Glut.glutDisplayFunc(Redraw);
-           // Glut.glutTimerFunc(0, Timer, 0);
+            Glut.glutTimerFunc(0, Timer, 0);
 
             Glut.glutMainLoop();
         }
@@ -53,10 +66,10 @@ namespace lb4_Alexander
 
             Gl.glMatrixMode(Gl.GL_PROJECTION);
             Gl.glLoadIdentity();
-            Glu.gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+            Glu.gluPerspective(140.0f, aspect, 0.1f, 100.0f);
             Gl.glMatrixMode(Gl.GL_MODELVIEW);
             Gl.glLoadIdentity();
-            Glu.gluLookAt(15f, 15f, 15.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+            Glu.gluLookAt(1f, 3f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);// пользователь смотрит с точки  1 3 5
         }
 
         static void Redraw()
@@ -66,14 +79,42 @@ namespace lb4_Alexander
 
             Axis3D.DrawAxis();
 
-            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, ambient);
-            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, mat_diffuse);
+            FigureMotion();
 
+            // Установка материала шара
+             Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light1);
+             Gl.glLightfv(Gl.GL_LIGHT1, Gl.GL_POSITION, light2);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, ambient);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, diffuse);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, specular);
+            Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_EMISSION, emission);
+            Gl.glMaterialf(Gl.GL_FRONT, Gl.GL_SHININESS, shininess);
+
+            // Отрисовка шара
             Glut.glutSolidSphere(3.0, 50, 50); // шар с радиусом 3
 
 
             Gl.glPopMatrix();
             Glut.glutSwapBuffers(); // Переключение буфферов отрисовки
+
+            CalculateNewFigureSize();
+        }
+
+        static void Timer(int value)
+        {
+            Glut.glutPostRedisplay();
+            Glut.glutTimerFunc(0, Timer, 0);
+        }
+
+        static void CalculateNewFigureSize()
+        {
+            _angleSphereY += 1;
+        }
+
+        static void FigureMotion()
+        {
+            // Вращение фигуры вокруг Y
+            Gl.glRotatef(_angleSphereY, 0f, 1.0f, 0f);
         }
 
         static void Main(string[] args)
